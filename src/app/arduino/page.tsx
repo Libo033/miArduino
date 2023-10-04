b2734540983a3e48f6343ed7ed8dd879eec24ffb
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-10-04 16:21:04 -0300

    Route Arduino en proceso

--- src/app/arduino/page.tsx
+++ src/app/arduino/page.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "@/app/arduino/page.module.css";
 
 const Arduino = () => {
-  return <div>Arduino</div>;
+  return (
+    <main className={styles.page}>
+      <section className={styles.section1}>
+        <h1>Modelos de Arduino</h1>
+        <p>
+          Una de las grandes cualidades de Arduino es su adaptabilidad, esto
+          significa que para cada situación que se nos presente podemos
+          conseguir una placa de Arduino que se adapte para cumplir con la tarea
+          de la manera mas eficientemente posible, a continuación te mostramos
+          los modelos mas populares de Arduino.
+        </p>
+      </section>
+      <section className={styles.section2}>
+
+      </section>
+    </main>
+  );
 };
 
 export default Arduino;
