61a73550e1e8933ff5ca36d2f2f3130e4f18690f
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-10-11 11:42:18 -0300

    Pagina de Proyectos [id] en proceso

--- src/app/proyectos/page.tsx
+++ src/app/proyectos/page.tsx
@@ -8,40 +8,27 @@ const Proyectos = () => {
   const [proyectos, setProyectos] = useState<ICardProject[]>([]);
 
   useEffect(() => {
-    fetch("/api/proyectos")
+    const controller = new AbortController();
+    const { signal } = controller;
+
+    fetch("/api/proyectos", { signal })
       .then((res) => res.json())
       .then((data) => setProyectos(data))
       .catch((err) => {
-        if (err instanceof Error) {
+        if (err instanceof Error && err.name !== "AbortError") {
           console.log(err.message);
         }
       });
+
+    return () => controller.abort();
   }, []);
 
   return (
     <main className={styles.page}>
       <h1>Proyectos</h1>
       <section className={styles.cardContainer}>
-        <CardProject
-          _id=""
-          name={"Trabajo Practico Valentin Libonati"}
-          image={
-            "https://res.cloudinary.com/dsuydyqgz/image/upload/v1696531567/03-arduino/lkayszeeyyabcyhceghn.png"
-          }
-          url={""}
-          info={""}
-        />
         {proyectos.length > 0 &&
-          proyectos.map((pr) => (
-            <CardProject
-              key={pr._id}
-              _id={pr._id}
-              name={pr.name}
-              image={pr.image}
-              url={pr.url}
-              info={pr.info}
-            />
-          ))}
+          proyectos.map((pr) => <CardProject key={pr._id} {...pr} />)}
       </section>
     </main>
   );
