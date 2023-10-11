d9d6ebc2ff9c658ef48ddcd788486f992f5b75e1
Libo033 <113865453+Libo033@users.noreply.github.com>
2023-10-11 14:16:00 -0300

    Nueva API Route GET / ID

--- src/app/api/proyectos/[id]/route.ts
+++ src/app/api/proyectos/[id]/route.ts
@@ -1,6 +1,21 @@
 import clientPromise from "@/libs/MongoConnect";
 import { Db, MongoClient, ObjectId } from "mongodb";
 
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const client: MongoClient = await clientPromise;
+  const db: Db = client.db("arduino");
+  const id: string = params.id;
+
+  const proyecto = await db
+    .collection("proyectos")
+    .findOne({ _id: new ObjectId(id) });
+
+  return Response.json(proyecto, { status: 200 });
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: { id: string } }
